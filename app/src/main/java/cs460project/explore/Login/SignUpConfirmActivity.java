2fd7f6e7ed5ba668835de42b23a8936101ecdb6c
Lin Hilgendorff <hilgend_lin@bentley.edu>
2017-03-21 16:31:04 -0400

    Created views for Confirm Account and Forgot Password

--- app/src/main/java/cs460project/explore/Login/SignUpConfirmActivity.java
+++ app/src/main/java/cs460project/explore/Login/SignUpConfirmActivity.java
@@ -1,8 +1,21 @@
 package cs460project.explore.Login;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import cs460project.explore.R;
+
 /**
  * Created by Kiley on 2/23/17.
  */
 
-public class SignUpConfirmActivity {
+public class SignUpConfirmActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+
+    }
+
 }
